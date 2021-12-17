652ae8e805e47e8d96b228e110c493314dc5192d
Zk408DevLoper <93755736+Zk408DevLoper@users.noreply.github.com>
2021-12-17 09:14:31 +0800

    Update Bruh.php

--- src/Bruh/Bruh.php
+++ src/Bruh/Bruh.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace Bruh;
+
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+
+class Bruh extends PluginBase implements Listener{
+	//When Plugin Enabled
+	public function onEnable(){
+		$this->getLogger()->info("JSKSJSKSHZKSJSKJSSKJSJSJSJSJSJSJSKSKEKEKWKEOOSODODODID");
+        //when plugin is disabled
+	}
+	public function onDisable(){
+		$this->getLogger()->info("JSKSJSKSHZKSJSKJSSKJSJSJSJSJSJSJSKSKEKEKWKEOOSODODODID");
+	}
+}
